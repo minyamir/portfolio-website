7955e4b1f9e9768d66ab762493328af6898b752c
Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>
2025-10-30 22:16:36 +0300

    Add Head component with meta tags and icons
    Signed-off-by: Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/me.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/me.png" />
+      <meta name="theme-color" content="#0f172a" />
+    </>
+  )
+}
