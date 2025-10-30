1c2b568e84f822ba402c0616b252060ebe4731d0
Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>
2025-10-30 22:25:27 +0300

    Update head.tsx
    Signed-off-by: Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,14 @@
 export default function Head() {
   return (
     <>
+      {/* Favicon shown in browser tab */}
       <link rel="icon" href="/me.png" type="image/png" />
+      {/* Apple touch icon for iOS */}
       <link rel="apple-touch-icon" href="/me.png" />
+      {/* Browser toolbar color */}
       <meta name="theme-color" content="#0f172a" />
+      {/* Title */}
+      <title>Minyamir Kelemu - Fullstack Developer</title>
     </>
   )
 }
-
-
-
