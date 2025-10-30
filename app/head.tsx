e26bd2c892383f77c2531712bb0c381015440d97
Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>
2025-10-30 22:26:56 +0300

    Update comments and reorder favicon and meta tags
    Signed-off-by: Minyamir Kelemu Adgeh <minyamirkelemu12@gmail.com>

--- app/head.tsx
+++ app/head.tsx
@@ -1,14 +1,13 @@
 export default function Head() {
   return (
     <>
-      {/* Favicon shown in browser tab */}
+      {/* Browser favicon */}
       <link rel="icon" href="/me.png" type="image/png" />
       {/* Apple touch icon for iOS */}
       <link rel="apple-touch-icon" href="/me.png" />
-      {/* Browser toolbar color */}
-      <meta name="theme-color" content="#0f172a" />
-      {/* Title */}
+      {/* Tab title */}
       <title>Minyamir Kelemu - Fullstack Developer</title>
+      <meta name="theme-color" content="#0f172a" />
     </>
   )
 }
